0652c3ac7142f212b9f3bcfc86b31fab78f24d03
Patrick Harris <patricklewery.harris@mail.utoronto.ca>
2017-12-21 15:24:22 -0500

    Changed mv to cp, fixed issues with python not executing

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ echo "Installing to $GOLDENDIR ..."
 mkdir $GOLDENDIR
 chmod +x complex-alias
 chmod +x script-maker.py
-mv complex-alias $GOLDENDIR/complex-alias
-mv script-maker.py $GOLDENDIR/script-maker.py
+cp complex-alias $GOLDENDIR/complex-alias
+cp script-maker.py $GOLDENDIR/script-maker.py
 echo "Installation complete. Make sure to add $GOLDENDIR to your PATH"
